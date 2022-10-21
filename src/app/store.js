9abd95231274f396c3910be1f53e38c30febeac3
Jason Bean <JBean@Flood.local>
2022-10-21 14:15:45 -0500

    removed unused vars

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import {  weatherApis, mapApi } from '../api/apiSlice';
 
